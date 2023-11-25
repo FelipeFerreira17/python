dbf8aaf8e05e8abb78aa4d506675fb0c25146bf8
Felipe Ferreira <felipe.fs.flp3@gmail.com>
2023-11-25 15:19:48 -0300

    Melhorando o exercicio anterior

--- exerciciopython1.py
+++ exerciciopython1.py
@@ -2,4 +2,4 @@ dia = input ("Digite o dia que você nasceu:")
 mes = input ("Digite o mês do seu nascimento:")
 ano = input ("Digite o ano que você nasceu:")
 
-print ("Sua data de nascimento é: ",dia,"/",mes,"/",ano)
+print ("Sua data de nascimento é: ",dia, "de ",mes, "de" ,ano) 
