93267768f710f0586f2eb86f3e2123f95287837a
Felipe Ferreira <felipe.fs.flp3@gmail.com>
2023-11-26 13:50:27 -0300

    Melhorando o exercicio

--- exerciciopython1.py
+++ exerciciopython1.py
@@ -2,4 +2,4 @@ numero1 = int( input ("Digite um numero:"))
 numero2 = int( input ("Digite o segundo numero: "))
 resultado = numero1 + numero2
 
-print ("A soma dos numeros é: ",resultado)
+print ("A soma entre {} mais {} é: {}".format(numero1, numero2, resultado))
