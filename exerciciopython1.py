c9e3a41224570513e5b94ab1566b64f3797da5c6
Felipe Ferreira <felipe.fs.flp3@gmail.com>
2023-11-27 00:21:56 -0300

    Fazendo exercicios

--- exerciciopython1.py
+++ exerciciopython1.py
@@ -1,3 +1,7 @@
-nome = str(input("Digite seu nome: "))
-print ('Você digitou %s' %(nome))
-print ('Seu nome é: %s' %(nome))
+# Criando uma média escolar
+nome = str(input('Digite o nome do aluno: '))
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+n3 = float(input('Digite a terceira nota: '))
+media = (n1+n2+n3) / 3
+print ('A media do aluno %s é: %2.1f' %(nome, media))
