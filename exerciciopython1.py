839d5078255c7c5b9baf1888da19f20a04df5e14
Felipe Ferreira <felipe.fs.flp3@gmail.com>
2023-12-01 00:56:00 -0300

    Exercicio de aumento de salário

--- exerciciopython1.py
+++ exerciciopython1.py
@@ -1,6 +1,8 @@
-# Converter metros em milimetros
+# Aumento de salário.
 
-metros = float(input('Digite o valor em metros: '))
-milimetros = metros * 1000
-
-print ('O valor de %2.1f em milimetros é: %5.2f' %(metros, milimetros)) 
+salario = float(input('Digite o valor do seu salário: '))
+aumento = int(input('Digite o percentual de aumento do salário: '))
+calculo = (aumento / 100) + 1
+novosalario = salario * calculo
+print ('Seu salário teve um aumento de %d por cento ' %(aumento))
+print ('Seu novo salário é de R$ %5.2f' %(novosalario))
