92fffe4fbdd473081e43d116679009544bbda1c1
Felipe Ferreira <felipe.fs.flp3@gmail.com>
2023-12-10 14:42:20 -0300

    Calculo de salario com IF

--- exerciciopython1.py
+++ exerciciopython1.py
@@ -1,6 +1,13 @@
-# Carro alugado
+salario = float(input('Digite o salário: '))
+base = salario
+aumento = 0
 
-dias = int(input('Digite a quantidade de dias que o carro foi alugado: '))
-kms = float(input('Digite os KMs percorridos pelo carro: '))
-calculo = (dias * 60) + (kms * 0.15)
-print ('O valor a pagar pelo carro alugado é de: R$ %5.2F' %(calculo))
+if base > 1250.00:
+    aumento = aumento + ((10/100) +1) 
+    base = base * aumento
+    print('Seu novo saário é de: %5.2f' %(base))
+
+if base <= 1250.00:
+    aumento = aumento + ((15/100)+1)
+    base = base * aumento
+    print('Seu novo salário é de: %5.2f' %(base))
