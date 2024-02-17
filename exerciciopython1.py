8b81bd235888f15661c3e8f90f64e0fcaffe0c43
Felipe Ferreira <felipe.fs.flp3@gmail.com>
2024-02-17 14:32:03 -0300

    tabuada de um numero

--- exerciciopython1.py
+++ exerciciopython1.py
@@ -1,28 +1,15 @@
-# Risando string
-# String começa a contar do zero. Estudando python a posição zero é o "E", a 1 é "S" e assim por diante.
-frase = 'Estudando python'
+#Tabuada
+numero = int(input('Digite um numero: '))
 
-# Fatiar
-print(frase[2]) # Posição dois é T. 
-print(frase[5]) # Posição cinco é A.
-print(frase[2:10]) # Da posição dois ate a 9. O numero depois do dois pontos vai mostrar o anterior. 
-print(frase[1:7]) # Da posição 1 até a 6. 
-print(frase[:6]) # Quando não tem nenhum número antes do dois pontos, começa a mostrar a posição zero.
-print(frase[8:]) # Quanto não tem nenhum numero depois do dois pontos, vai do que tá antes dos dois pontos ao final.
-print(frase[4:9:2]) # Da posição 4 até a 8 pulando de dois em dois.
-print(frase[:12:3]) # Da posição zero até a 11 pulando de 3 em 3.
-print(frase[::2]) # Do inicio até o final pulando de 2 em 2.
-print(frase.count('o')) # count conta quantas vezes algo aparece. nesse caso o "o" minusculo
-print(frase.count('E')) # mostra quanta vezes o "E" maiusculo aparece.
-print(frase.upper()) # upper() transforma em maiusculo.
-print(frase.upper().count('A')) # Transma em maiuscula e conta quantas vezes aparece a ketra "A" maiuscula.
-print(len(frase)) # len() mostra o tamanho da string.
-print(len(frase.strip())) # mostra o tamanho da string e o strip() remove os espaços indesejados antes e depois.
-print(frase.replace('python','android')) # replace() troca uma palavra pela outra. Mas não vai salvar. só altera nessa parte.
-frase = frase.replace('python','programação') # atribuindo, ai sim faz a alteração na raiz.
-print(frase)
-print(frase.find('estudando')) # find() procura uma palavra que você digitou. Senão encontrar va aparecer -1
-print(frase.lower()) #lower() transforma em minuscula.
-print(frase.split()) # divide a string.
-dividir = frase.split()
-print(dividir)
+print('--------------')
+print(numero, 'X 1 = ', numero*1)
+print(numero, 'X 2 = ', numero*2)
+print(numero, 'X 3 = ', numero*3)
+print(numero, 'X 4 = ', numero*4)
+print(numero, 'X 5 = ', numero*5)
+print(numero, 'X 6 = ', numero*6)
+print(numero, 'X 7 = ', numero*7)
+print(numero, 'X 8 = ', numero*8)
+print(numero, 'X 9 = ', numero*9)
+print(numero, 'X 10 = ', numero*10)
+print('--------------')
